ee44b57607482e535b3747e1aa94ef1dde5394da
Lagrango <davidnlagrange@hotmail.com>
2020-01-21 15:36:45 -0600

    Added GetCompany Method to Companies Controller + Created EmployeesController

--- ASP.NET_Core_Web_API/Controllers/CompaniesController.cs
+++ ASP.NET_Core_Web_API/Controllers/CompaniesController.cs
@@ -1,13 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
 using Contracts;
 using Entities.DataTransferObjects;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Repository;
 
 namespace ASP.NET_Core_Web_API.Controllers
 {
@@ -27,9 +23,24 @@ namespace ASP.NET_Core_Web_API.Controllers
         [HttpGet]
         public IActionResult GetCompanies()
         {
-                var companies = _repository.Company.GetAllCompanies(trackChanges: false);
-                var companiesDto = _mapper.Map<IEnumerable<CompanyDto>>(companies);
-                return Ok(companiesDto);
+            var companies = _repository.Company.GetAllCompanies(trackChanges: false);
+            var companiesDto = _mapper.Map<IEnumerable<CompanyDto>>(companies);
+            return Ok(companiesDto);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetCompany(Guid id)
+        {
+            var company = _repository.Company.GetCompany(id, trackChanges: false);
+            if(company == null)
+            {
+                _logger.LogInfo($"Company with id: {id} doesn't exist in the database.");
+                return NotFound();
+            }
+            else
+            {
+                var companyDto = _mapper.Map<CompanyDto>(company);
+                return Ok(companyDto);
+            }
         }
     }
 }
---
+++ ASP.NET_Core_Web_API/Controllers/EmployeesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Core_Web_API.Controllers
+{
+    [Route("api/companies/{companyId}/employees")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IRepositoryManager _context;
+        private readonly ILoggerManager _logger;
+        private readonly IMapper _mapper;
+        public EmployeesController(IRepositoryManager context, ILoggerManager logger, IMapper mapper)
+        {
+            _context = context;
+            _logger = logger;
+            _mapper = mapper;
+        }
+    }
+}
