524e7f6eb36f67b51ec79c08295e7de3153aa077
Lagrango <davidnlagrange@hotmail.com>
2020-01-22 10:00:03 -0600

    Added Custom CSV Formatt Option for Response Formatt

--- ASP.NET_Core_Web_API/Extensions/ServiceExtensions.cs
+++ ASP.NET_Core_Web_API/Extensions/ServiceExtensions.cs
@@ -35,5 +35,8 @@ namespace ASP.NET_Core_Web_API.Extensions
 
         public static void ConfigureRepositoryManager(this IServiceCollection services) =>
             services.AddScoped<IRepositoryManager, RepositoryManager>();
+
+        public static IMvcBuilder AddCustomCSVFormatter(this IMvcBuilder builder) =>
+            builder.AddMvcOptions(config => config.OutputFormatters.Add(new CsvOutputFormatter()));
     }
 }
--- ASP.NET_Core_Web_API/Startup.cs
+++ ASP.NET_Core_Web_API/Startup.cs
@@ -36,7 +36,8 @@ namespace ASP.NET_Core_Web_API
             {
                 config.RespectBrowserAcceptHeader = true;
                 config.ReturnHttpNotAcceptable = true;
-            }).AddXmlDataContractSerializerFormatters();
+            }).AddXmlDataContractSerializerFormatters()
+              .AddCustomCSVFormatter();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
