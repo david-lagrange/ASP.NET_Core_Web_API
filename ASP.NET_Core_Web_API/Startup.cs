697b7dabc609b9a5b4111039a7a76929aba7ae9f
Lagrango <davidnlagrange@hotmail.com>
2020-01-22 09:03:01 -0600

    Added XML Response Option + Return 406 If Requested Formatt Is Not Supported

--- ASP.NET_Core_Web_API/Startup.cs
+++ ASP.NET_Core_Web_API/Startup.cs
@@ -32,7 +32,11 @@ namespace ASP.NET_Core_Web_API
             services.ConfigureSqlContext(Configuration);
             services.ConfigureRepositoryManager();
             services.AddAutoMapper(typeof(Startup));
-            services.AddControllers();
+            services.AddControllers(config =>
+            {
+                config.RespectBrowserAcceptHeader = true;
+                config.ReturnHttpNotAcceptable = true;
+            }).AddXmlDataContractSerializerFormatters();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
