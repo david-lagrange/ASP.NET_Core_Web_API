7cfc48ceb3e058fe3e6294d6352d8770068133e0
Lagrango <davidnlagrange@hotmail.com>
2020-01-24 14:15:43 -0600

    Completed Delete Company and Children + Working on Put For Employee

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -23,5 +23,6 @@ namespace Repository
         public void CreateCompany(Company company) => Create(company);
         public IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges) =>
             FindByCondition(c => ids.Contains(c.Id), trackChanges).ToList();
+        public void DeleteCompany(Company company) => Delete(company);
     }
 }
